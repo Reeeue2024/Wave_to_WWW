48e39875e4fe4a9eacf18c6eafde809cb4fd6a96
xyznn <hjseo0819@dankook.ac.kr>
2025-06-29 23:46:30 +0900

    [ Deploy ] Update start_docker.sh

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -5,5 +5,8 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$SCRIPT_DIR/.."
 
+# .env 파일 S3에서 가져오기
+aws s3 cp s3://wavetowww-bucket/prod/.env "$PROJECT_ROOT/source/server/app/.env"
+
 # 멀티 컨테이너 재시작
-docker-compose -f "$PROJECT_ROOT/source/docker-compose.yml" start
+docker-compose -f "$PROJECT_ROOT/source/docker-compose.yml" up -d --build
