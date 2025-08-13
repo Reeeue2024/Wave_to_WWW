d7bf4f6a2890a7ee7c1c964c48bb6d892c7291b9
xyznn <hjseo0819@dankook.ac.kr>
2025-08-13 22:22:22 +0900

    [ Deploy ] Update copy_env.sh

--- scripts/copy_env.sh
+++ scripts/copy_env.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-cp /home/ubuntu/.env /home/ubuntu/wavetowww/source/server/app/.env
+echo "[INFO] Copying backend.env to server/app/.env"
+cp /home/ubuntu/env-configs/backend.env /home/ubuntu/app/source/server/app/.env
+
+echo "[INFO] Copying kernel.env to kernel/.env"
+cp /home/ubuntu/env-configs/kernel.env /home/ubuntu/app/source/kernel/.env
+
+echo "[INFO] All .env files copied successfully."
